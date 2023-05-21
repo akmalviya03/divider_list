38cba930113cb3505c335216f4bbb0e3cf008e89
akmalviya03 <abhishakkrmalviya@gmail.com>
2023-05-21 11:31:28 +0530

    added properties for customisation of ListView

--- lib/divider_listview.dart
+++ lib/divider_listview.dart
@@ -8,11 +8,36 @@ class DividerListView extends AbstractDividerWidget {
       super.itemIfDividerListLengthIsShort,
       super.itemIfMainListLengthIsShort,
       required super.mainListItemLength,
-      required super.dividerListItemLength});
-
+      required super.dividerListItemLength,
+      this.controller,
+      this.reverse = false,
+      this.shrinkWrap = false,
+      this.primary,
+      this.physics,
+      this.padding,
+      this.scrollDirection = Axis.vertical,
+      this.keyboardDismissBehavior = ScrollViewKeyboardDismissBehavior.manual,
+      this.clipBehavior = Clip.hardEdge});
+  final ScrollController? controller;
+  final bool reverse;
+  final bool shrinkWrap;
+  final bool? primary;
+  final ScrollPhysics? physics;
+  final EdgeInsets? padding;
+  final Axis scrollDirection;
+  final ScrollViewKeyboardDismissBehavior keyboardDismissBehavior;
+  final Clip clipBehavior;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      scrollDirection: scrollDirection,
+      keyboardDismissBehavior: keyboardDismissBehavior,
+      padding: padding,
+      reverse: reverse,
+      controller: controller,
+      physics: physics,
+      shrinkWrap: shrinkWrap,
+      primary: primary,
       itemBuilder: getWidget,
       itemCount: _itemCount(),
     );
